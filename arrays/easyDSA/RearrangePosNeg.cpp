5848157fd05186cbe7b6062e978d971589f6795b
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-08-29 00:22:14 +0530

    28Aug

--- arrays/easyDSA/RearrangePosNeg.cpp
+++ arrays/easyDSA/RearrangePosNeg.cpp
@@ -1,38 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> rearrange(vector<int> arr)
+vector<int> rearrange(vector<int> &arr)
 {
     int n = arr.size();
-    vector<int> ans(n,0);
-    int count =0;
-    for(int i=0;i<n;i++)
+    vector<int> arr1;
+    vector<int> arr2;
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i]>0)
+        if (arr[i] >= 0)
         {
-            count++;
+            arr1.push_back(arr[i]);
+        }
+        else
+        {
+            arr2.push_back(arr[i]);
         }
     }
-    for(int i=0;i<n;i++)
+    int i = 0;
+    int j = 0, k = 0;
+    while (i < n)
     {
-       if(arr[i]>=0)
-       {
-            ans[2*i] = arr[i];
-       }
-       else
-       {
-        ans[(i)+1] = arr[i];
-       }
-        
+
+        arr[i] = arr1[j];
+        i++;
+        j++;
+        arr[i] = arr2[k];
+        i++;
+        k++;
     }
-    return ans;
+    return arr;
 }
 int main()
 {
-    vector<int> arr = {1,2,-4,-5};
+    vector<int> arr = {1, 2, -4, -5};
     vector<int> finalArray = rearrange(arr);
-    for(int i =0;i<finalArray.size();i++)
+    for (int i = 0; i < finalArray.size(); i++)
     {
-        cout<<finalArray[i]<<" ";
+        cout << finalArray[i] << " ";
     }
     return 0;
 }
