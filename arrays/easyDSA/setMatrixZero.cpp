b027937ae98e33e18b2f40d311b65fdfa92d8617
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-09-03 22:03:13 +0530

    3Sept

--- arrays/easyDSA/setMatrixZero.cpp
+++ arrays/easyDSA/setMatrixZero.cpp
@@ -1,18 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
+int zeroes(vector<vector<int>> arr)
 {
-    vector<vector<int>> arr = {{1,1,1},{1,0,1},{1,1,1}};
+    int count = 0;
     int n = arr.size();
     int m = arr[0].size();
-    cout<<"initial matrix : "<<endl;
-    for(int i =0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<m;j++)
+        for (int j = 0; j < m; j++)
         {
-            cout<<arr[i][j]<<" ";
+            if (arr[i][j] == 0)
+            {
+                count++;
+            }
         }
-        cout<<endl;
+    }
+    return count;
+}
+vector<int> index(vector<vector<int>> arr)
+{
+    vector<int> index;
+    int n = arr.size();
+    int m = arr[0].size();
+    int count = zeroes(arr);
+    for (int k = 0; k < count; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (arr[i][j] == 0)
+                {
+                    index.push_back(i);
+                    index.push_back(j);
+                }
+                return index;
+            }
+        }
+    }
+
+}
+int main()
+{
+    vector<vector<int>> arr = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = arr.size();
+    int m = arr[0].size();
+    cout << "initial matrix : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    vector<int> idx = index(arr);
+    int a = idx[0];
+    int b = idx[1];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i][a] = 0;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        arr[b][i] = 0;
+    }
+    cout << "modified matrix : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
