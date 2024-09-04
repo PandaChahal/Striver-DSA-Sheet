b422c743b3aa622f7120dd60b9d2ab1fdb491a89
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-09-04 23:03:40 +0530

    4Sept

---
+++ arrays/easyDSA/upperbound.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> arr, int k)
+{
+    int n = arr.size();
+    int l = 0;
+    int r = n - 1;
+    int index = n;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if(arr[mid] >= k)
+        {
+            index = mid;
+            r = mid -1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return index;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int x = 6;
+    int ind = binarySearch(arr, x);
+    cout <<"index : "<<ind;
+    return 0;
+}
