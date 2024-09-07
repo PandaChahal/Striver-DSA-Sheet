4444f46b673f513e2e8779379ae7405e4bcfec94
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-09-07 21:22:38 +0530

    7Sept

--- arrays/easyDSA/majorityelementnby2.cpp
+++ arrays/easyDSA/majorityelementnby2.cpp
@@ -1,26 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int majorityElement(vector<int> v) 
 { 
 	int n = v.size();
-	int max = *max_element(v.begin(),v.end());
-	vector<int> hash(max+1,0);
-	for(int i =0;i<n;i++)
+	int e = v[0];
+	int count =1;
+	for (int i=1;i<n;i++)
 	{
-		hash[v[i]]++;
-	} 
-	for(int i =0;i<max+1;i++)
-	{
-		if(hash[i]>(n/2))
+		if(v[i] == e)
 		{
-			return i;
+			count++;
+		}
+		else if (v[i]!=0)
+		{
+			count--;
+		}
+		if(count == 0)
+		{
+			e = v[i];
+			count = 1;
 		}
 	}
-	return-1;
+	int cnt = 0;
+    for (int i = 0; i < n; i++) 
+	{
+        if (v[i] == e) 
+		{
+			cnt++;
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(cnt > (n/2))
+		{
+			return e;
+		}
+	}
+	return -1;
 }
 int main()
 {
     vector<int> v = {4,4,2,4,3,4,4,3,2,4};
-    cout<<majorityElement(v);
+    cout<<"majority element in the above given array is "<<majorityElement(v);
     return 0;
 }
