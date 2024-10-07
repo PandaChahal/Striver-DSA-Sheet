073413a20cc05cd5d6f8eefc7f132a69f31095b2
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-07 22:32:28 +0530

    revised optimal

--- arrays/easyDSA/onceOccuring.cpp
+++ arrays/easyDSA/onceOccuring.cpp
@@ -3,25 +3,31 @@ using namespace std;
 int check(vector<int> arr)
 {
     int n = arr.size();
-    int max = *max_element(arr.begin(),arr.end());
-    int size = max + 1;
-    vector<int> hash(size,0);
+    // int max = *max_element(arr.begin(),arr.end());
+    // int size = max + 1;
+    // vector<int> hash(size,0);
+    // for(int i =0;i<n;i++)
+    // {
+    //     hash[arr[i]]++;
+    // }
+    // for(int i=0;i<size;i++)
+    // {
+    //     if(hash[i]==1)
+    //     {
+    //         return i;
+    //     }
+    // }
+    // return -1;
+    int xorr = 0;
     for(int i =0;i<n;i++)
     {
-        hash[arr[i]]++;
+        xorr = xorr ^ arr[i];
     }
-    for(int i=0;i<size;i++)
-    {
-        if(hash[i]==1)
-        {
-            return i;
-        }
-    }
-    return -1;
+    return xorr;
 }
 int main()
 {
-    vector<int> arr ={4,1,2,1,2};
+    vector<int> arr ={4,1,2,1,2,4,7,8,9,8,9};
     cout<<check(arr);
     return 0;
 }
