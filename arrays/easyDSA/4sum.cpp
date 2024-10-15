f1516713ebaf6b3b30bee48ba3fbac9fd2f67d74
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-15 23:25:19 +0530

    15 oct

--- arrays/easyDSA/4sum.cpp
+++ arrays/easyDSA/4sum.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> triplet( vector<int> &arr,int target)
+//brute force
+/*vector<vector<int>> triplet( vector<int> &arr,int target)
 {
     int n = arr.size();
     vector<vector<int>> ans;
@@ -27,13 +28,58 @@ vector<vector<int>> triplet( vector<int> &arr,int target)
         }
     }
     return ans;
+}*/
+
+//optimal 
+vector<vector<int>> Quad(vector<int>& arr, int target)
+{
+    int n = arr.size(); 
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) 
+    {
+        if (i > 0 && arr[i] == arr[i - 1]) 
+        {
+            continue;
+        }
+        for (int j = i + 1; j < n; j++) 
+        {
+            if (j > i + 1 && arr[j] == arr[j - 1]) 
+            {
+                continue;
+            }
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) 
+            {
+                int sum = arr[i] + arr[j]+arr[k]+arr[l];
+                if(sum > target)
+                {
+                    l--;
+                }
+                else if(sum < target)
+                {
+                    k++;
+                }
+                else
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++; l--;
+                    while (k < l && arr[k] == arr[k - 1]) k++;
+                    while (k < l && arr[l] == arr[l + 1]) l--;
+                }
+            }
+        }
+    }
+    return ans;
 }
 int main()
 {
     vector<int> arr = { 4,3,3,4,4,2,1,2,1,1};
     int target = 9;
     int n = arr.size();
-    vector<vector<int>> ans = triplet( arr,target);
+    vector<vector<int>> ans = Quad( arr,target);
     for (int i =0;i<ans.size();i++) 
     {
         for(int j=0;j<ans[i].size();j++)
@@ -42,7 +88,6 @@ int main()
         }
         cout<<endl;
     }
-    
     return 0;
 }
 
