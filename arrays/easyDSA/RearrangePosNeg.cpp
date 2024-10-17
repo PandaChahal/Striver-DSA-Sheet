059f25114100b0526845c8d9f5522dfb7fece544
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-17 22:57:06 +0530

    17 Oct

--- arrays/easyDSA/RearrangePosNeg.cpp
+++ arrays/easyDSA/RearrangePosNeg.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> modifier(vector<int> arr)
+/*vector<int> modifier(vector<int> arr)
 {
     int n = arr.size();
     vector<int> pos;
@@ -35,6 +35,27 @@ vector<int> modifier(vector<int> arr)
         }
     }
     return arr;
+}*/
+vector<int> modifier(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> ans(n,0);
+    int pos = 0;
+    int neg = 1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<0)
+        {
+            ans[neg] = arr[i];
+            neg += 2;
+        }
+        else
+        {
+            ans[pos] = arr[i];
+            pos += 2;
+        }
+    }
+    return ans;
 }
 int main()
 {
