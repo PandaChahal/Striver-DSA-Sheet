fbefb2f35d48800b3c251758776fd293e87f674f
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-19 17:44:41 +0530

    19 Oct

--- LinkedLists/deleteNthlastNode.cpp
+++ LinkedLists/deleteNthlastNode.cpp
@@ -1,18 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
 {
-    public:
-    int data ;
-    Node* next;
+public:
+    int data;
+    Node *next;
 
-    Node(int value , Node* next1)
+    Node(int value, Node *next1)
     {
         data = value;
         next = next1;
     }
-    
+
     Node(int value)
     {
         data = value;
@@ -20,34 +20,34 @@ class Node
     }
 };
 
-void printLList(Node* n) 
+void printLList(Node *n)
 {
-    while (n != nullptr) 
+    while (n != nullptr)
     {
-        cout << n->data<<" ";
+        cout << n->data << " ";
         n = n->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-Node* reverseRecursive(Node* head)
+Node *reverseRecursive(Node *head)
 {
-    if(head == nullptr || head->next == nullptr)
+    if (head == nullptr || head->next == nullptr)
     {
         return head;
     }
-    Node* newhead = reverseRecursive(head->next);
-    Node* front = head->next;
+    Node *newhead = reverseRecursive(head->next);
+    Node *front = head->next;
     front->next = head;
     head->next = nullptr;
     return newhead;
 }
-Node* deleteNode(Node* head, int n)
-{   
+Node *deleteNode(Node *head, int n)
+{
     int count = n;
     Node *a = head;
-    Node * b;
-    while(count != 1)
+    Node *b;
+    while (count != 1)
     {
         b = a;
         a = a->next;
@@ -59,22 +59,22 @@ Node* deleteNode(Node* head, int n)
 }
 int main()
 {
-    Node* n1 = new Node(10);
-    Node* n2 = new Node(20);
-    Node* n3 = new Node(30);
-    Node* n4 = new Node(40);
+    Node *n1 = new Node(10);
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(30);
+    Node *n4 = new Node(40);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
-    int x ;
-    cout<<"enter the node to be deleted : " ;
-    cin>>x;
-    cout<<"original LL :";
+    int x;
+    cout << "enter the node to be deleted : ";
+    cin >> x;
+    cout << "original LL :";
     printLList(n1);
     n1 = reverseRecursive(n1);
-    n1 = deleteNode(n1,x);
+    n1 = deleteNode(n1, x);
     n1 = reverseRecursive(n1);
-    cout<<"modified LL :";
+    cout << "modified LL :";
     printLList(n1);
     return 0;
 }
