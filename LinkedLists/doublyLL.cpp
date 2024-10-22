14395dc88a9343fd8fbafc7ea415437121fd89fc
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-22 22:46:16 +0530

    21 Oct

--- LinkedLists/doublyLL.cpp
+++ LinkedLists/doublyLL.cpp
@@ -1,11 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
-    int data ;
-    Node* next;
-    Node* prev;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
 
     Node(int value)
     {
@@ -13,7 +14,7 @@ class Node{
         next = nullptr;
         prev = nullptr;
     }
-    Node(int value,Node* next1,Node* prev1)
+    Node(int value, Node *next1, Node *prev1)
     {
         data = value;
         next = next1;
@@ -21,37 +22,37 @@ class Node{
     }
 };
 
-void printDLL(Node* head)
+void printDLL(Node *head)
 {
-    while(head != nullptr)
+    while (head != nullptr)
     {
-        cout<<head->data<<" ";
+        cout << head->data << " ";
         head = head->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-Node* insertNode(int value,Node* head)
+Node *insertNode(int value, Node *head)
 {
-    Node* a = new Node(value);
+    Node *a = new Node(value);
     a->next = head;
     return a;
 }
-Node* deleteNode(Node* head)
+Node *deleteNode(Node *head)
 {
-    Node* a = head;      
-    head = head->next;    
-    head->prev = nullptr;   
-    a->next = nullptr;  
-    return head;  
+    Node *a = head;
+    head = head->next;
+    head->prev = nullptr;
+    a->next = nullptr;
+    return head;
 }
 
 int main()
 {
-    Node* n1 = new Node(10);
-    Node* n2 = new Node(20);
-    Node* n3 = new Node(30);
-    Node* n4 = new Node(40);
+    Node *n1 = new Node(10);
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(30);
+    Node *n4 = new Node(40);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
@@ -59,10 +60,10 @@ int main()
     n3->prev = n2;
     n4->prev = n3;
     printDLL(n1);
-    //inserting a new node at head
-    n1 = insertNode(50,n1);
+    // inserting a new node at head
+    n1 = insertNode(50, n1);
     printDLL(n1);
-    //deleting head node
+    // deleting head node
     n1 = deleteNode(n1);
     printDLL(n1);
     return 0;
