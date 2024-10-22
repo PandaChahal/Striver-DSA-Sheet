73fc5c2cd1fb716a01b018d794ece3a016ed310d
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-22 22:46:43 +0530

    21 Oct

--- LinkedLists/add2LL.cpp
+++ LinkedLists/add2LL.cpp
@@ -1,88 +1,88 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
-    public:
-    int data ;
-    Node* next;
+public:
+    int data;
+    Node *next;
     Node(int value)
     {
         data = value;
         next = nullptr;
     }
-    Node(int value,Node * next1)
+    Node(int value, Node *next1)
     {
         data = value;
         next = next1;
     }
 };
 
-Node* sum(Node* l1,Node* l2)
+Node *sum(Node *l1, Node *l2)
 {
-    Node * temp1 = l1;
-    Node * temp2 = l2;
-    Node * dummynode = new Node(-1);
-    Node * current = dummynode;
+    Node *temp1 = l1;
+    Node *temp2 = l2;
+    Node *dummynode = new Node(-1);
+    Node *current = dummynode;
     int carry = 0;
-    while(temp1 != nullptr || temp2 != nullptr)
+    while (temp1 != nullptr || temp2 != nullptr)
     {
         int sum = carry;
-        if(temp1)
+        if (temp1)
         {
             sum = sum + temp1->data;
         }
-        if(temp2)
+        if (temp2)
         {
             sum = sum + temp2->data;
         }
-        Node * newnode = new Node(sum%10);
-        carry = sum/10;
+        Node *newnode = new Node(sum % 10);
+        carry = sum / 10;
         current->next = newnode;
         current = current->next;
-        if(temp1)
+        if (temp1)
         {
             temp1 = temp1->next;
         }
-        if(temp2)
+        if (temp2)
         {
             temp2 = temp2->next;
         }
     }
-    if(carry)
+    if (carry)
     {
-        Node * newnode = new Node (carry);
+        Node *newnode = new Node(carry);
         current->next = newnode;
     }
     return dummynode->next;
 }
-void printLList(Node* n) 
+void printLList(Node *n)
 {
-    while (n != nullptr) 
+    while (n != nullptr)
     {
-        cout << n->data<<" ";
+        cout << n->data << " ";
         n = n->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int main()
 {
-    Node* l1 = new Node(2);
-    Node* l11 = new Node(4);
-    Node* l12 = new Node(3);
+    Node *l1 = new Node(2);
+    Node *l11 = new Node(4);
+    Node *l12 = new Node(3);
     l1->next = l11;
     l11->next = l12;
-    Node* l2 = new Node(5);
-    Node* l21 = new Node(6);
-    Node* l22 = new Node(7);
+    Node *l2 = new Node(5);
+    Node *l21 = new Node(6);
+    Node *l22 = new Node(7);
     l2->next = l21;
     l21->next = l22;
-    cout<<"first LL is ";
+    cout << "first LL is ";
     printLList(l1);
-    cout<<"second LL is ";
+    cout << "second LL is ";
     printLList(l2);
-    Node* s1 = sum(l1,l2);
-    cout<<"sum is ";
+    Node *s1 = sum(l1, l2);
+    cout << "sum is ";
     printLList(s1);
     return 0;
 }
