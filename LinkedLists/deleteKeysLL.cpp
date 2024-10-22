90fe73ccea9b44f87f55a0a8805d77c980d21921
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-22 22:45:39 +0530

    21 Oct

--- LinkedLists/deleteKeysLL.cpp
+++ LinkedLists/deleteKeysLL.cpp
@@ -1,18 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
-    public:
-    int data ;
-    Node* next;
-    Node* prev;
+public:
+    int data;
+    Node *next;
+    Node *prev;
     Node(int value)
     {
         data = value;
         next = nullptr;
         prev = nullptr;
     }
-    Node(int value,Node * next1,Node* prev1)
+    Node(int value, Node *next1, Node *prev1)
     {
         data = value;
         next = next1;
@@ -22,9 +22,9 @@ class Node
 
 Node *deleteOccurances(Node *head, int key)
 {
-    Node * nextNode = head;
-    Node * prevNode = nullptr;
-    Node * temp = head;
+    Node *nextNode = head;
+    Node *prevNode = nullptr;
+    Node *temp = head;
     while (temp != nullptr)
     {
         if (temp->data == key)
@@ -53,24 +53,24 @@ Node *deleteOccurances(Node *head, int key)
     }
     return head;
 }
-void printLList(Node* n) 
+void printLList(Node *n)
 {
-    while (n != nullptr) 
+    while (n != nullptr)
     {
-        cout << n->data<<" ";
+        cout << n->data << " ";
         n = n->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int main()
 {
-    Node* n1 = new Node(10);
-    Node* n2 = new Node(20);
-    Node* n3 = new Node(10);
-    Node* n4 = new Node(40);
-    Node* n5 = new Node(50);
-    Node* n6 = new Node(60);
+    Node *n1 = new Node(10);
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(10);
+    Node *n4 = new Node(40);
+    Node *n5 = new Node(50);
+    Node *n6 = new Node(60);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
@@ -82,7 +82,7 @@ int main()
     n5->prev = n4;
     n6->prev = n5;
     printLList(n1);
-    n1 = deleteOccurances(n1,10);
+    n1 = deleteOccurances(n1, 10);
     printLList(n1);
     return 0;
 }
