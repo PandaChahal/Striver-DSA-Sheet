616583b74f38b73db3ad0bd18b61bd2f8a50a509
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-10-23 21:20:58 +0530

    21 Oct

--- LinkedLists/palindrome.cpp
+++ LinkedLists/palindrome.cpp
@@ -1,53 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
-    public:
-    int data ;
-    Node* next;
+public:
+    int data;
+    Node *next;
     Node(int value)
     {
         data = value;
         next = nullptr;
     }
-    Node(int value,Node * next1)
+    Node(int value, Node *next1)
     {
         data = value;
         next = next1;
     }
 };
-Node* reverseLL(Node* head)
+Node *reverseLL(Node *head)
 {
-    if(head == nullptr || head->next == nullptr)
+    if (head == nullptr || head->next == nullptr)
     {
         return head;
     }
-    Node* newhead = reverseLL(head->next);
-    Node* front = head->next;
+    Node *newhead = reverseLL(head->next);
+    Node *front = head->next;
     front->next = head;
     head->next = nullptr;
     return newhead;
 }
 
-bool palindromeCheck(Node* head)
+bool palindromeCheck(Node *head)
 {
-    if(head == nullptr || head->next == nullptr)
+    if (head == nullptr || head->next == nullptr)
     {
         return head;
     }
-    Node* slow = head;
-    Node* fast = head;
-    while(fast->next != nullptr && fast->next->next != nullptr)
+    Node *slow = head;
+    Node *fast = head;
+    while (fast->next != nullptr && fast->next->next != nullptr)
     {
         slow = slow->next;
         fast = fast->next->next;
     }
-    Node* newhead = reverseLL(slow->next);
-    Node* first = head;
-    Node* second = newhead;
-    while(second != NULL)
+    Node *newhead = reverseLL(slow->next);
+    Node *first = head;
+    Node *second = newhead;
+    while (second != NULL)
     {
-        if(first->data != second->data)
+        if (first->data != second->data)
         {
             reverseLL(newhead);
             return false;
@@ -58,35 +58,37 @@ bool palindromeCheck(Node* head)
     reverseLL(newhead);
     return true;
 }
-void printLList(Node* n) 
+void printLList(Node *n)
 {
-    while (n != nullptr) 
+    while (n != nullptr)
     {
-        cout << n->data<<" ";
+        cout << n->data << " ";
         n = n->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int main()
 {
-    Node* n1 = new Node(1);
-    Node* n2 = new Node(5);
-    Node* n3 = new Node(2);
-    Node* n4 = new Node(5);
-    Node* n5 = new Node(1);
+    Node *n1 = new Node(1);
+    Node *n2 = new Node(5);
+    Node *n3 = new Node(2);
+    Node *n4 = new Node(5);
+    Node *n5 = new Node(1);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
     n4->next = n5;
-    cout<<"linked list is ";
+    cout << "linked list is ";
     printLList(n1);
     bool k = palindromeCheck(n1);
-    if(k){
-        cout<<"given LL is a palindrome";
+    if (k)
+    {
+        cout << "given LL is a palindrome";
     }
-    else{
-        cout<<"its not a palindrome ";
+    else
+    {
+        cout << "its not a palindrome ";
     }
     return 0;
 }
