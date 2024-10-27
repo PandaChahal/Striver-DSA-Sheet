a7cc9227c176de73d67841333392632b87b516c9
PandaChahal <hkaur4_be22@thapar.edu>
2024-10-27 14:57:39 +0530

    27 October

--- test/test3.cpp
+++ test/test3.cpp
@@ -1,21 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+// class Node
+// {
+//     public:
+//     int data;
+//     Node * next;
+//     Node (int value)
+//     {
+//         data = value;
+//         next = nullptr;
+//     }
+//     Node(int value,Node* next1)
+//     {
+//         data = value;
+//         next = next1;
+//     }
+// };
+
 class Node
 {
     public:
-    int data;
+    int data ;
     Node * next;
+    Node * prev;
     Node (int value)
     {
         data = value;
         next = nullptr;
+        prev = nullptr;
     }
-    Node(int value,Node* next1)
+    Node (int value,Node * next1,Node* prev1)
     {
         data = value;
         next = next1;
+        prev = prev1;
     }
 };
+
 void printLL(Node* head)
 {
     Node * n = head;
@@ -140,61 +161,124 @@ Node * sum2(Node * n1,Node * n2)
 //     return 0;
 // }
 
-Node * sort(Node * head)
+//sort 0,1,2
+// Node * sort(Node * head)
+// {
+//     if(head == NULL || head->next == NULL)
+//     {
+//         return head;
+//     }
+//     Node * temp = head;
+//     Node * zeroHead = new Node(-1);
+//     Node * oneHead = new Node(-1);
+//     Node * twoHead = new Node(-1);
+//     Node * zero = zeroHead;
+//     Node * one = oneHead;
+//     Node * two = twoHead;
+//     while(temp != NULL)
+//     {
+//         if(temp->data == 0)
+//         {
+//             zero->next = temp;
+//             zero = temp;
+//         }
+//         else if(temp->data == 1)
+//         {
+//             one->next = temp;
+//             one = temp;
+//         }
+//         else
+//         {
+//             two->next = temp;
+//             two = temp;
+//         }
+//         temp = temp->next;
+//     }
+//     zero->next = (oneHead->next) ? oneHead->next : twoHead->next;
+//     one->next = twoHead->next;
+//     two->next = NULL;
+//     Node * newHead = zeroHead->next;
+//     return newHead;
+// }
+
+// int main()
+// {
+//     Node* n1 = new Node (0);
+//     Node* n2 = new Node (2);
+//     Node* n3 = new Node (1);
+//     Node* n4 = new Node (2);
+//     Node* n5 = new Node (1);
+//     Node* n6 = new Node (0);
+//     n1->next = n2;
+//     n2->next = n3;
+//     n3->next = n4;
+//     n4->next = n5;
+//     n5->next = n6;
+//     cout<<"original LL is ";
+//     printLL(n1);
+//     n1 = sort(n1);
+//     cout<<"now the LL is ";
+//     printLL(n1);
+//     return 0;
+// }
+
+Node* deleteKeys(Node* head,int key)
 {
-    if(head == NULL || head->next == NULL)
-    {
-        return head;
-    }
+    Node * nextNode = head;
+    Node * prevNode = nullptr;
     Node * temp = head;
-    Node * zeroHead = new Node(-1);
-    Node * oneHead = new Node(-1);
-    Node * twoHead = new Node(-1);
-    Node * zero = zeroHead;
-    Node * one = oneHead;
-    Node * two = twoHead;
-    while(temp != NULL)
+    while(temp != nullptr)
     {
-        if(temp->data == 0)
+        if(temp->data == key)
         {
-            zero->next = temp;
-            zero = temp;
-        }
-        else if(temp->data == 1)
-        {
-            one->next = temp;
-            one = temp;
+            if(temp == head)
+            {
+                head = head->next;
+            }
+            nextNode = temp->next;
+            prevNode = temp->prev;
+            if(nextNode)
+            {
+                nextNode->prev = prevNode;
+            }
+            if(prevNode)
+            {
+                prevNode->next = nextNode;
+            }
+            free(temp);
+            temp = nextNode;
         }
         else
         {
-            two->next = temp;
-            two = temp;
+            temp = temp->next;
         }
-        temp = temp->next;
     }
-    zero->next = (oneHead->next) ? oneHead->next : twoHead->next;
-    one->next = twoHead->next;
-    two->next = NULL;
-    Node * newHead = zeroHead->next;
-    return newHead;
+    return head;
 }
-
 int main()
 {
-    Node* n1 = new Node (0);
-    Node* n2 = new Node (2);
-    Node* n3 = new Node (1);
-    Node* n4 = new Node (2);
-    Node* n5 = new Node (1);
-    Node* n6 = new Node (0);
+    Node* n1 = new Node(1);
+    Node* n2 = new Node(2);
+    Node* n3 = new Node(3);
+    Node* n4 = new Node(1);
+    Node* n5 = new Node(1);
+    Node* n6 = new Node(4);
+    Node* n7 = new Node(10);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
     n4->next = n5;
     n5->next = n6;
+    n6->next = n7;
+    n2->prev = n1;
+    n3->prev = n2;
+    n4->prev = n3;
+    n5->prev = n4;
+    n6->prev = n5;
+    n7->prev = n6;
     cout<<"original LL is ";
     printLL(n1);
-    n1 = sort(n1);
+    n1 = deleteKeys(n1,1);
     cout<<"now the LL is ";
     printLL(n1);
     return 0;
