86efc913c860809a158a6bb5d8f72e24b230c50a
PandaChahal <hkaur4_be22@thapar.edu>
2024-10-28 21:58:38 +0530

    28 October

---
+++ stack&Queue/queueFromArray.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+    int front;
+    int back;
+    int* arr;
+    int n=10;
+
+    public:
+    Queue()
+    {
+        front = -1;
+        back = -1;
+        arr = new int[n];
+    }
+    
+    void push(int val)
+    {
+        if(back==(n-1))
+        {
+            cout<<"queue is full"<<endl;
+            return;
+        }
+        if(front==-1)
+        {
+            front++;
+        }
+        back++;
+        arr[back]=val;
+    }
+
+    void pop()
+    {
+        if(front==-1 || front>back)
+        {
+            cout<<"The queue is empty"<<endl;
+            return;
+        }
+        front++;
+    }
+
+    int top()
+    {
+        if(front==-1 || front>back)
+        {
+            cout<<"The queue is empty"<<endl;
+            return -1;
+        }
+        return front;
+    }
+
+    int size()
+    {
+        if(front==-1 || front>back)
+        {
+            cout<<"The queue is empty"<<endl;
+            return -1;
+        }
+        return back;
+    }
+};
+
+int main()
+{
+    Queue q1;
+    q1.push(1);
+    q1.push(2);
+    q1.push(3);
+    q1.push(4);
+    cout<<q1.top()<<" ";
+    q1.pop();
+    cout<<q1.top()<<" ";
+    q1.pop();
+    cout<<q1.top()<<" ";
+    q1.pop();
+    cout<<q1.top()<<" ";
+    return 0;
+}
