74055c6181560047715dcd4546bfe340ac949cbb
PandaChahal <hkaur4_be22@thapar.edu>
2024-10-30 22:49:11 +0530

    30 October

--- stack&Queue/queueFromStack.cpp
+++ stack&Queue/queueFromStack.cpp
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class MyQueue 
+class Queue 
 {
   public:
-    stack < int > input, output;
-  MyQueue() {
+  stack < int > input, output;
+
+  Queue() 
+  {
 
   }
 
-  void push(int x) {
-    cout << "The element pushed is " << x << endl;
+  void push(int x) 
+  {
     input.push(x);
   }
 
-  int pop() {
-    
+  int pop()
+  {    
     if (output.empty())
+    {
       while (input.size())
+      {
         output.push(input.top()), input.pop();
-
+      }
+    }    
     int x = output.top();
     output.pop();
     return x;
@@ -27,8 +32,12 @@ class MyQueue
 
   int top() {
     if (output.empty())
+    {
       while (input.size())
+      {
         output.push(input.top()), input.pop();
+      }
+    }
     return output.top();
   }
 
@@ -38,11 +47,14 @@ class MyQueue
 
 };
 int main() {
-  MyQueue q;
+  Queue q;
+  q.push(1);
+  q.push(2);
   q.push(3);
   q.push(4);
-  cout << "The element poped is " << q.pop() << endl;
   q.push(5);
   cout << "The top of the queue is " << q.top() << endl;
+  q.pop();
+  cout<<"now the top element is "<<q.top()<<endl;
   cout << "The size of the queue is " << q.size() << endl;
 }
