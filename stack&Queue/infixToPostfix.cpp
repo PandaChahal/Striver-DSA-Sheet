ff92351f9a0ab81a3e0e7b3813366b01ebfc6dde
PandaChahal <hkaur4_be22@thapar.edu>
2024-11-06 11:58:37 +0530

    striver

---
+++ stack&Queue/infixToPostfix.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+int precd(char c)
+{
+    if(c == '^')
+    {
+        return 3;
+    }
+    else if (c == '/' || c =='*')
+    {
+        return 2;
+    }
+    else if(c == '+' || c =='-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+void infixToPostfix(string s)
+{
+    stack<int> st;
+    string result;
+    for(int i=0;i<s.length();i++)
+    {
+        char c = s[i];
+        if(c >= 'a' && c<='z' || c >= 'A' && c<='Z' || c >= '0' && c<='9')
+        {
+            result += c;
+        }
+        else if(c == '(')
+        {
+            st.push('(');
+        }
+        else if(c == ')')
+        {
+            while(st.top() != '(')
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+        {
+            while(!(st.empty()) && precd(c) < precd(st.top()))
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+    cout<<result<<endl;
+}
+int main()
+{
+    string s = "(p+q)*(m-n)";
+    infixToPostfix(s);
+    return 0;
+}
