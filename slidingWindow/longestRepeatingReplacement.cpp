ec5bf205b225ef8246e67d704a9b56c251314ab3
PandaChahal <hkaur4_be22@thapar.edu>
2024-11-11 22:48:24 +0530

    11-11-2024

---
+++ slidingWindow/longestRepeatingReplacement.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxLength(string s,int k)
+{
+    vector<int> hash(26,0);
+    int r=0,l=0;
+    int maxF = 0,maxl = 0;
+    while(r < s.length())
+    {
+        hash[s[r]-'a']++;
+        maxF = max(maxF,hash[s[r]-'a']);
+        while(((r-l+1)-maxF)>k)
+        {
+            hash[s[l]]--;
+            maxF = 0;
+        }
+        if(((r-l+1)-maxF) <= k)
+        {
+            maxl = max(maxl,(r-l+1));
+        }
+        r++;
+    }
+    return maxl;
+}
+int main()
+{
+    string s = "abab";
+    cout<<maxLength(s,2);
+    return 0;
+}
