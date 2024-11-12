fd3591943202af6c03889eb68c21c08ec5126ef1
PandaChahal <hkaur4_be22@thapar.edu>
2024-11-12 22:36:12 +0530

    12-11-2024

---
+++ slidingWindow/subArrayKDistinct.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int KDistinct(vector<int> arr,int k)
+{
+    map<int,int> mp;
+    int l=0,r=0,count=0;
+    while(r < arr.size())
+    {
+        mp[arr[r]]++;
+        while(mp.size() > k)
+        {
+            mp[arr[l]]--;
+            if(mp[arr[l]] == 0)
+            {
+                mp.erase(arr[l]);
+            }
+            l++;
+        }
+        if(mp.size() <= k)
+        {
+            count += (r-l+1);
+        }
+        r++;
+    }
+    return count;
+}
+int main()
+{
+    vector<int> arr = {1,2,1,2,3};
+    cout<<(KDistinct(arr,2)-KDistinct(arr,1));
+    return 0;
+}
