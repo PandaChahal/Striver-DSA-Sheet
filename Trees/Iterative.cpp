83949e58ecdf349451a23e40d3bb538d8131d802
PandaChahal <hkaur4_be22@thapar.edu>
2024-11-17 23:13:49 +0530

    15-11-2024

--- Trees/Iterative.cpp
+++ Trees/Iterative.cpp
@@ -1,14 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    Node * right ;
-    Node * left;
+    Node *right;
+    Node *left;
     Node(int value)
     {
         right = left = nullptr;
-        data = value ;
+        data = value;
     }
 };
 
@@ -38,54 +39,59 @@ vector<int> preOrder(Node *root)
     return preOrder;
 }
 
-vector<int>inorder(Node* root){
-    vector<int>ans;
-    stack<Node*>st;
-    Node* dummy = root;
-    while(true){
-        if(dummy!=NULL){
+vector<int> inorder(Node *root)
+{
+    vector<int> ans;
+    stack<Node *> st;
+    Node *dummy = root;
+    while (true)
+    {
+        if (dummy != NULL)
+        {
             st.push(dummy);
-            dummy=dummy->left;
+            dummy = dummy->left;
         }
-        else{
-            if(st.empty()){
-                 break;
+        else
+        {
+            if (st.empty())
+            {
+                break;
             }
             dummy = st.top();
             st.pop();
             ans.push_back(dummy->data);
-            dummy=dummy->right;
-
+            dummy = dummy->right;
         }
     }
     return ans;
-    
-    
 }
-vector<int> postOrder(Node* root) 
+vector<int> postOrder(Node *root)
 {
     vector<int> postorder;
 
-    if(root == NULL){
+    if (root == NULL)
+    {
         return postorder;
     }
 
-    stack<Node*> st1, st2;
+    stack<Node *> st1, st2;
     st1.push(root);
-    while(!st1.empty())
+    while (!st1.empty())
     {
         root = st1.top();
         st1.pop();
         st2.push(root);
-        if(root->left != NULL){
+        if (root->left != NULL)
+        {
             st1.push(root->left);
         }
-        if(root->right != NULL){
+        if (root->right != NULL)
+        {
             st1.push(root->right);
         }
     }
 
-    while(!st2.empty())
+    while (!st2.empty())
     {
         postorder.push_back(st2.top()->data);
         st2.pop();
@@ -93,29 +99,28 @@ vector<int> postOrder(Node* root)
     return postorder;
 }
 
-
 int main()
 {
-    Node* root = new Node(1);
+    Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
     vector<int> ans = preOrder(root);
-    for(int i=0;i<ans.size();i++)
+    for (int i = 0; i < ans.size(); i++)
     {
-        cout<<ans[i]<<" ";
+        cout << ans[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
     vector<int> ans2 = postOrder(root);
-    for(int i=0;i<ans2.size();i++)
+    for (int i = 0; i < ans2.size(); i++)
     {
-        cout<<ans2[i]<<" ";
+        cout << ans2[i] << " ";
     }
     vector<int> ans3 = inorder(root);
-    for(int i=0;i<ans3.size();i++)
+    for (int i = 0; i < ans3.size(); i++)
     {
-        cout<<ans3[i]<<" ";
+        cout << ans3[i] << " ";
     }
     return 0;
 }
