77370b60d4cf8b1224205c8a542af19e46e40bb5
PandaChahal <hkaur4_be22@thapar.edu>
2025-02-17 22:00:22 +0530

    dsa

--- Trees/hard/allInOne.cpp
+++ Trees/hard/allInOne.cpp
@@ -13,6 +13,51 @@ struct Node
     }
 };
 
+vector<vector<int>> allInOne(Node * root)
+{
+    stack<pair<Node*,int>> st;
+    st.push({root, 1});
+    vector<int> pre, in, post;
+    if(root == NULL) return {};
+    
+    while(!st.empty())
+    {
+        auto i = st.top();
+        st.pop();
+        
+        // Preorder
+        if(i.second == 1)
+        {
+            pre.push_back(i.first->data);
+            i.second++;
+            st.push(i);
+            
+            if(i.first->left != NULL)
+            {
+                st.push({i.first->left, 1});
+            }
+        }
+        // Inorder
+        else if(i.second == 2)
+        {
+            in.push_back(i.first->data);
+            i.second++;
+            st.push(i);
+            
+            if(i.first->right != NULL)
+            {
+                st.push({i.first->right, 1});
+            }
+        }
+        // Postorder
+        else
+        {
+            post.push_back(i.first->data);
+        }
+    }
+    
+    return {pre, in, post};
+}
 
 int main()
 {
@@ -21,5 +66,19 @@ int main()
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    
+    vector<vector<int>> result = allInOne(root);
+    cout << "Preorder: ";
+    for(int val : result[0]) cout << val << " ";
+    cout << endl;
+
+    cout << "Inorder: ";
+    for(int val : result[1]) cout << val << " ";
+    cout << endl;
+
+    cout << "Postorder: ";
+    for(int val : result[2]) cout << val << " ";
+    cout << endl;
+
     return 0;
 }
