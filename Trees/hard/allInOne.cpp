836a3b0291bd5e9f1fabd9dff9da6fdf8b6784e2
PandaChahal <hkaur4_be22@thapar.edu>
2025-03-10 12:54:12 +0530

    dsa

--- Trees/hard/allInOne.cpp
+++ Trees/hard/allInOne.cpp
@@ -6,45 +6,46 @@ struct Node
     int data;
     Node *left;
     Node *right;
-    Node(int val) 
+    Node(int val)
     {
         right = left = NULL;
         data = val;
     }
 };
 
-vector<vector<int>> allInOne(Node * root)
+vector<vector<int>> allInOne(Node *root)
 {
-    stack<pair<Node*,int>> st;
+    stack<pair<Node *, int>> st;
     st.push({root, 1});
     vector<int> pre, in, post;
-    if(root == NULL) return {};
-    
-    while(!st.empty())
+    if (root == NULL)
+        return {};
+
+    while (!st.empty())
     {
         auto i = st.top();
         st.pop();
-        
+
         // Preorder
-        if(i.second == 1)
+        if (i.second == 1)
         {
             pre.push_back(i.first->data);
             i.second++;
             st.push(i);
-            
-            if(i.first->left != NULL)
+
+            if (i.first->left != NULL)
             {
                 st.push({i.first->left, 1});
             }
         }
         // Inorder
-        else if(i.second == 2)
+        else if (i.second == 2)
         {
             in.push_back(i.first->data);
             i.second++;
             st.push(i);
-            
-            if(i.first->right != NULL)
+
+            if (i.first->right != NULL)
             {
                 st.push({i.first->right, 1});
             }
@@ -55,7 +56,7 @@ vector<vector<int>> allInOne(Node * root)
             post.push_back(i.first->data);
         }
     }
-    
+
     return {pre, in, post};
 }
 
@@ -66,18 +67,21 @@ int main()
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    
+
     vector<vector<int>> result = allInOne(root);
     cout << "Preorder: ";
-    for(int val : result[0]) cout << val << " ";
+    for (int val : result[0])
+        cout << val << " ";
     cout << endl;
 
     cout << "Inorder: ";
-    for(int val : result[1]) cout << val << " ";
+    for (int val : result[1])
+        cout << val << " ";
     cout << endl;
 
     cout << "Postorder: ";
-    for(int val : result[2]) cout << val << " ";
+    for (int val : result[2])
+        cout << val << " ";
     cout << endl;
 
     return 0;
