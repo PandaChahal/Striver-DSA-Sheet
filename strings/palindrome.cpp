6e724bf943440a8c328e6639cbd9a9f6e0fa67d9
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-02 21:52:23 +0530

    commited

--- strings/palindrome.cpp
+++ strings/palindrome.cpp
@@ -5,6 +5,9 @@ int main()
 {
     string s = "this is a random string";
     cout<<"Before reversing the string is : "<<s;
-
+    s+=" ";
+    stack<string> st;
+    int i;
+    string str="";
     return 0;
 }
