57edbbe2508f95951f9fcbfca86f3a94f922005d
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-10 16:46:42 +0530

    commited

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <vector>
 using namespace std;
+
 vector<int> bfs(vector<vector<int>> arr, int n)
 {
     vector<int> ans;
@@ -40,3 +41,78 @@ int main()
     }
     return 0;
 }
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int n = adj.size();
+        queue<int> q;
+        vector<int> vis(n+1,0);
+        q.push(0);
+        vis[0] = 1;
+        vector<int> ans;
+        while(!q.empty())
+        {
+            int node = q.front();
+            ans.push_back(node);
+            q.pop();
+            for(auto it : adj[node])
+            {
+                if(!vis[it])
+                {
+                    q.push(it);
+                    vis[it] = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// int main() {
+//     int tc;
+//     cin >> tc;
+//     cin.ignore();
+//     while (tc--) {
+//         int V;
+//         cin >> V;
+//         cin.ignore();
+//         // Use vector of vectors instead of array of vectors.
+//         vector<vector<int>> adj(V);
+
+//         for (int i = 0; i < V; i++) {
+//             string input;
+//             getline(cin, input);
+//             int num;
+//             vector<int> node;
+//             stringstream ss(input);
+//             while (ss >> num) {
+//                 node.push_back(num);
+//             }
+//             adj[i] = node;
+//         }
+
+//         Solution obj;
+//         vector<int> ans = obj.bfs(adj);
+//         for (int i = 0; i < ans.size(); i++) {
+//             cout << ans[i] << " ";
+//         }
+//         cout << endl;
+//         cout << "~" << endl;
+//     }
+//     return 0;
+// }
+
+// } Driver Code Ends
