e449ec3d76eeeeec20702c8eb12a1850823dc73a
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-16 20:23:54 +0530

    commited

--- graphs/floodfill.cpp
+++ graphs/floodfill.cpp
@@ -1,36 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int row, int col, vector<vector<int>> &ans,vector<vector<int>> &image, int newColor, int delRow[], int delCol[],int iniColor)
+void dfs(vector<vector<int>> &matrix, int sr, int sc, int prevColor, int color, vector<vector<int>> &vis)
 {
-    ans[row][col] = newColor;
-    int n = image.size();
-    int m = image[0].size();
+    int n = matrix.size();
+    int m = matrix[0].size();
+    matrix[sr][sc] = color;
+    vis[sr][sc] = 1;
+    int drow[4] = {1, 0, -1, 0};
+    int dcol[4] = {0, 1, 0, -1};
     for (int i = 0; i < 4; i++)
     {
-        int nrow = row + delRow[i];
-        int ncol = col + delCol[i];
-        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor)
+        int nrow = sr + drow[i];
+        int ncol = sc + dcol[i];
+
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+            matrix[nrow][ncol] == prevColor && vis[nrow][ncol] == -1)
         {
-            dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);
+            dfs(matrix, nrow, ncol, prevColor, color, vis);
         }
     }
 }
 
-vector<vector<int>> floodFill(vector<vector<int>> &image,int sr, int sc, int newColor)
+vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
 {
-    int iniColor = image[sr][sc];
-    vector<vector<int>> ans = image;
-    int delRow[] = {-1, 0, +1, 0};
-    int delCol[] = {0, +1, 0, -1};
-    dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
-    return ans;
+    vector<vector<int>> vis(image.size(), vector<int>(image[0].size(), -1));
+    vector<vector<int>> matrix = image;
+    int prevColor = image[sr][sc];
+    dfs(matrix, sr, sc, prevColor, color, vis);
+
+    return matrix;
 }
 
 int main()
 {
-
-    vector<vector<int>> image{{1, 1, 1},{1, 1, 0},{1, 0, 1}};
+    vector<vector<int>> image{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
     vector<vector<int>> ans = floodFill(image, 1, 1, 2);
     for (auto i : ans)
     {
