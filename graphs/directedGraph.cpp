6531cb2b278a1cad5e3f48d651058ecc25ffc339
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-19 18:44:12 +0530

    commited

--- graphs/directedGraph.cpp
+++ graphs/directedGraph.cpp
@@ -1,34 +1,51 @@
-class Solution {
-    private:
-     bool dfs(vector<int> adj[],vector<int> &vis,vector<int> &pathVis,int node)
-     {
-         vis[node] =1;
-         pathVis[node] =1 ;
-         for(auto it:adj[node])
-         {
-             if(!vis[it])
-             {
-                 if(dfs(adj,vis,pathVis,it)) return true;
-             }
-             else if(pathVis[it])
-             {
-                 return true;
-             }
-         }
-         pathVis[node] = 0;
-         return false;
-     }
-    public:
-     bool isCyclic(int N, vector<int> adj[]) {
-         vector<int> vis(N,0);
-         vector<int> pathVis(N,0);
-         for(int i=0;i<N;i++)
-         {
-             if(!vis[i])
-             {
-                 return dfs(adj,vis,pathVis,i);
-             }
-         }
-     }
- };
- 
+#include <bits/stdc++.h>
+using namespace std;
+
+bool dfs(vector<int> adj[], vector<int> &vis, vector<int> &pathVis, int node)
+{
+    vis[node] = 1;
+    pathVis[node] = 1;
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+        {
+            if (dfs(adj, vis, pathVis, it))
+                return true;
+        }
+        else if (pathVis[it])
+        {
+            return true;
+        }
+    }
+    pathVis[node] = 0;
+    return false;
+}
+
+bool isCyclic(int N, vector<int> adj[])
+{
+    vector<int> vis(N, 0);
+    vector<int> pathVis(N, 0);
+    for (int i = 0; i < N; i++)
+    {
+        if (!vis[i])
+        {
+            return dfs(adj, vis, pathVis, i);
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> adj[11] = {{}, {2}, {3}, {4, 7}, {5}, {6}, {}, {5}, {2,9}, {10}, {8}};
+    bool ans = isCyclic(11,adj);
+    if(ans)
+    {
+        cout<<"yes \n";
+    }
+    else
+    {
+        cout<<"no \n";
+    }
+    return 0;
+}
