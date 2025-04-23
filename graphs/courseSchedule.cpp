20864d773378f8041462a997726e2f3a18fea183
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-23 12:22:14 +0530

    commited

---
+++ graphs/courseSchedule.cpp
@@ -0,0 +1,40 @@
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> indegree(numCourses,0);
+    vector<vector<int>> adj(numCourses);
+    for(auto it:prerequisites)
+    {
+        adj[it[1]].push_back(it[0]);
+    }
+    for(int i=0;i<numCourses;i++)
+    {
+        for(auto it:adj[i])
+        {
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    vector<int> topo;
+    for(int i=0;i<numCourses;i++)
+    {
+        if(indegree[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+    while(!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto it:adj[node])
+        {
+            indegree[it]--;
+            if(indegree[it] == 0)
+            {
+                q.push(it);
+            }
+        }
+    }
+    if(topo.size() == numCourses) return true;
+    return false;
+}
