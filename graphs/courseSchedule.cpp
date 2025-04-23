14e49db7e5bf67fa9bc93d78fe50bd0de7de7681
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-23 22:56:20 +0530

    commited

--- graphs/courseSchedule.cpp
+++ graphs/courseSchedule.cpp
@@ -1,40 +1,70 @@
-bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    vector<int> indegree(numCourses,0);
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+{
+    vector<int> indegree(numCourses, 0);
     vector<vector<int>> adj(numCourses);
-    for(auto it:prerequisites)
+    for (auto it : prerequisites)
     {
         adj[it[1]].push_back(it[0]);
     }
-    for(int i=0;i<numCourses;i++)
+    for (int i = 0; i < numCourses; i++)
     {
-        for(auto it:adj[i])
+        for (auto it : adj[i])
         {
             indegree[it]++;
         }
     }
     queue<int> q;
     vector<int> topo;
-    for(int i=0;i<numCourses;i++)
+    for (int i = 0; i < numCourses; i++)
     {
-        if(indegree[i] == 0)
+        if (indegree[i] == 0)
         {
             q.push(i);
         }
     }
-    while(!q.empty())
+    while (!q.empty())
     {
         int node = q.front();
         q.pop();
         topo.push_back(node);
-        for(auto it:adj[node])
+        for (auto it : adj[node])
         {
             indegree[it]--;
-            if(indegree[it] == 0)
+            if (indegree[it] == 0)
             {
                 q.push(it);
             }
         }
     }
-    if(topo.size() == numCourses) return true;
+    if (topo.size() == numCourses)
+    {
+        return true;
+    }
     return false;
+}
+
+int main()
+{
+    vector<vector<int>> prerequisites;
+	int N = 4;
+	prerequisites.push_back({1, 0});
+	prerequisites.push_back({2, 1});
+	prerequisites.push_back({3, 2});
+
+	bool ans = canFinish(N, prerequisites);
+
+	if (ans) 
+    {
+        cout << "YES";
+    }
+	else 
+    {
+        cout << "NO";
+    }
+	cout << endl;
+
+	return 0;
 }
