391b295dce20817b473fbc7d8242af45f88457d2
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-25 22:38:26 +0530

    commited

--- graphs/safeState.cpp
+++ graphs/safeState.cpp
@@ -1,35 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-	vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+	vector<int> eventualSafeNodes(int V, vector<int> adj[])
+	{
 		vector<int> adjRev[V];
 		int indegree[V] = {0};
-		for (int i = 0; i < V; i++) {
+		for (int i = 0; i < V; i++)
+		{
 			// i -> it
 			// it -> i
-			for (auto it : adj[i]) {
+			for (auto it : adj[i])
+			{
 				adjRev[it].push_back(i);
 				indegree[i]++;
 			}
 		}
 		queue<int> q;
 		vector<int> safeNodes;
-		for (int i = 0; i < V; i++) {
-			if (indegree[i] == 0) {
+		for (int i = 0; i < V; i++)
+		{
+			if (indegree[i] == 0)
+			{
 				q.push(i);
 			}
 		}
 
-		while (!q.empty()) {
+		while (!q.empty())
+		{
 			int node = q.front();
 			q.pop();
 			safeNodes.push_back(node);
-			for (auto it : adjRev[node]) {
+			for (auto it : adjRev[node])
+			{
 				indegree[it]--;
-				if (indegree[it] == 0) q.push(it);
+				if (indegree[it] == 0)
+					q.push(it);
 			}
 		}
 
@@ -38,16 +46,16 @@ public:
 	}
 };
 
-int main() {
+int main()
+{
 
-	vector<int> adj[12] = {{1}, {2}, {3, 4}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10},
-		{8}, {9}
-	};
+	vector<int> adj[12] = {{1}, {2}, {3, 4}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10}, {8}, {9}};
 	int V = 12;
 	Solution obj;
 	vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
 
-	for (auto node : safeNodes) {
+	for (auto node : safeNodes)
+	{
 		cout << node << " ";
 	}
 	cout << endl;
