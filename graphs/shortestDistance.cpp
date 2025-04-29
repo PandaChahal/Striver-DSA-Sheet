b20592e1bf53f1f58fcbed5e701b96173b1d71f1
PandaChahal <hkaur4_be22@thapar.edu>
2025-04-29 18:58:32 +0530

    commited

--- graphs/shortestDistance.cpp
+++ graphs/shortestDistance.cpp
@@ -1,9 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<int> shortestPath(vector<vector<int>> &edges, int N, int M)
+{
+    vector<int> ans(N, INT_MAX);
+    vector<vector<int>> adj(N);
+
+    for (int i = 0; i < M; i++)
+    {
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]); 
+    }
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    ans[0] = 0;
+
+    while (!q.empty())
+    {
+        int node = q.front().first;
+        int dist = q.front().second;
+        q.pop();
+
+        for (auto it : adj[node])
+        {
+            if (ans[it] > dist + 1)
+            {
+                ans[it] = dist + 1;
+                q.push({it, dist + 1});
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (ans[i] == INT_MAX)
+            ans[i] = -1;
+    }
+
+    return ans;
+}
 int main()
 {
-    vector<vector<int>> grid{
-
+    vector<vector<int>> edges{
+        {0,1},{0,3},{3,4},{4,5},{5, 6},{1,2},{2,6},{6,7},{7,8},{6,8}
     };
+    vector<int> ans = shortestPath(edges,9,10);
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    return 0;
 }
---
+++ graphs/shortestPathDag.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> shortestPath(int N, int M, vector<vector<int>> &edges)
+{
+    vector<int> ans(N, INT_MAX);
+    vector<vector<pair<int, int>>> adj(N);
+
+    for (int i = 0; i < M; i++)
+    {
+        adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+    }
+
+    queue<pair<int,int>> q;
+    q.push({0, 0});
+    ans[0] = 0;
+
+    while (!q.empty())
+    {
+        int dist = q.front().first;
+        int node = q.front().second;
+        q.pop();
+
+        for (auto it : adj[node])
+        {
+            int nexNode = it.first;
+            int nextWt = it.second;
+            if (ans[nexNode] > dist + nextWt)
+            {
+                ans[nexNode] = dist + nextWt;
+                q.push({dist + nextWt, nexNode});
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (ans[i] == INT_MAX)
+        {
+            ans[i] = -1;
+        }   
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> edges{
+        {0,1,2},
+        {0,4,1},
+        {4,5,4},
+        {4,2,2},
+        {1,2,3},
+        {2,3,6},
+        {5,3,1}
+    };
+    vector<int> ans= shortestPath(6,7,edges);
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
