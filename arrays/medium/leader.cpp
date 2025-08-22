d79f9ca5f3821de2f97e2122121d0a7c05d9848a
PandaChahal <hkaur4_be22@thapar.edu>
2025-08-22 14:43:00 +0530

    revision

--- arrays/medium/leader.cpp
+++ arrays/medium/leader.cpp
@@ -1,29 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> printLeaders(int arr[], int n)
+vector<int> printLeaders(vector<int> nums)
 {
   vector<int> ans;
-  int max = arr[n - 1];
-  ans.push_back(arr[n - 1]);
+  int n = nums.size();
+  ans.push_back(nums[n - 1]);
+  int maxi = nums[n - 1];
+
   for (int i = n - 2; i >= 0; i--)
-    if (arr[i] > max)
+  {
+    if (nums[i] > maxi)
     {
-      ans.push_back(arr[i]);
-      max = arr[i];
+      ans.push_back(nums[i]);
+      maxi = nums[i];
     }
+  }
+
+  reverse(ans.begin(), ans.end());
   return ans;
 }
 
 int main()
 {
-  int n = 6;
-  int arr[n] = {10, 22, 12, 3, 0, 6};
-  vector<int> ans = printLeaders(arr, n);
-  for (int i = ans.size() - 1; i >= 0; i--)
-  {
-    cout << ans[i] << " ";
-  }
-  cout << endl;
-  return 0;
+	vector<int> nums = {10, 22, 12, 3, 0, 6};
+	vector<int> ans = printLeaders(nums);
+	for(int i = ans.size()-1; i>=0; i--)
+	{
+		cout<<ans[i]<<" ";
+	}
+
+	cout<<endl;
+	return 0;
 }
